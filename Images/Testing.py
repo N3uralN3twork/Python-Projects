5332ae72f25bf280ca14f608f7fd8e4f68d533a2
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-05-02 10:46:40 -0400

    Images

---
+++ Images/Testing.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+list = ["a", "a", "a", "b", "b", "b", "b","c", "c"]
+# Count number of items in a list
+len(list)
+# Count of unique items in list
+dict = {i:list.count(i) for i in list}
+dict
+
+
+
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

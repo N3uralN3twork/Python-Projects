d2f463c396a2eebb7c203be30b7634e34adbdf6e
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-05-06 20:18:11 -0400

    Summary

--- Coding Contests/Testing.py
+++ Coding Contests/Testing.py
@@ -1,10 +1,28 @@
-def integer_powers(a,b,c,d):
-    print((a**b)+(c**d))
+import bs4
+import requests
 
 
-if __name__=="__main__":
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    d = int(input())
-    integer_powers(a,b,c,d)
+def getFormFields(URL):
+    res = requests.get(URL)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.findAll('label')  # Attribute can be changed as per the element to be scraped
+    return elems
+
+
+def printElems(elems):
+    file = open('OnlineForm.docx', 'a')
+    for i in elems:
+        print(i.text.strip())
+        file.write(i.text.strip())
+        file.write('\n\n')
+    file.close()
+
+
+print('Enter the URL of the page you want to scrape')
+URL = input()
+# print('Enter the CSS selector you want to find on the page')
+# cssSelect = input()
+
+elems = getFormFields(URL)
+printElems(elems)

9c0f052945198b93990dbd534df22a1bd4301458
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-05-27 08:06:07 -0400

    NLP

---
+++ NLP/Toxic Comments/Toxic.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
+from tensorflow.keras.layers import Bidirectional, GlobalMaxPool1D
+from tensorflow.keras.models import Model
+from tensorflow.keras import initializers, regularizers, constraints, optimizers, layers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

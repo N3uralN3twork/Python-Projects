bbd2363ea09125d57085c7ab19674e9fe97b8bb1
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-02 11:32:45 -0400

    Update Testing.py

--- NLP/Testing.py
+++ NLP/Testing.py
@@ -1,4 +1,19 @@
-from transformers import pipeline
-nlp = pipeline(task = "sentiment-analysis")
-print(nlp("I love you"))
-print(nlp("Today was a really good day for me!"))
+import os
+import sys
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Dense, Input, GlobalMaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.models import Model
+from keras.initializers import Constant
+
+
+BASE_DIR = ''
+GLOVE_DIR = os.path.join(BASE_DIR, 'glove.6B')
+TEXT_DATA_DIR = os.path.join(BASE_DIR, '20_newsgroup')
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NUM_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2

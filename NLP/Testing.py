4e0a79d103d2a24a4ce5b100e9cab05682c02131
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-06 18:58:58 -0400

    Update Testing.py

---
+++ NLP/Testing.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import pandas as pd
+from sklearn import preprocessing
+import keras
+import numpy as np
+
+url = "https://tfhub.dev/google/elmo/3"
+embed = hub.load(url)
+
+
+
+
+
+
+
+

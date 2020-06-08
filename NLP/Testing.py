dfb278ac151b33c68e6088c51787330530850f9f
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-08 13:19:34 -0400

    Update Testing.py

--- NLP/Testing.py
+++ NLP/Testing.py
@@ -1,40 +1,7 @@
-from transformers import BertTokenizer, BertForSequenceClassification
-
-
-model = BertForSequenceClassification.from_pretrained(
-    "bert-base-uncased",
-    use_cdn=True,
-    num_labels = 2,
-    output_attentions = False,
-    output_hidden_states = False
-)
-
-model.cuda()
-
-sentences = train.Review.values
-labels = train.Sentiment.values
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case = True)
-
-# Tokenize the Dataset:
-
-max_length = 0
-for sent in sentences:
-    input_ids = tokenizer.encode(sent, add_special_tokens=True)
-    max_length = max(max_length, len(input_ids))
-print(f"Max sentence length: {max_length}")
-
-
-
-
-
-
-
-
-
-
-
-
-
+from gensim.models.fasttext import FastText
+embedding_size = 60
+window_size = 40
+min_word = 5
+down_sampling = 0.01
 
 

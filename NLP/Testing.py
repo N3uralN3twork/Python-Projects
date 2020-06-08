9c6f811d1043fd49d6204da75c5aa80c3eec47c3
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-08 00:39:42 -0400

    Update Testing.py

--- NLP/Testing.py
+++ NLP/Testing.py
@@ -1,4 +1,4 @@
-from transformers import BertForSequenceClassification, AdamW
+from transformers import BertTokenizer, BertForSequenceClassification
 
 
 model = BertForSequenceClassification.from_pretrained(
@@ -13,3 +13,28 @@ model.cuda()
 
 sentences = train.Review.values
 labels = train.Sentiment.values
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case = True)
+
+# Tokenize the Dataset:
+
+max_length = 0
+for sent in sentences:
+    input_ids = tokenizer.encode(sent, add_special_tokens=True)
+    max_length = max(max_length, len(input_ids))
+print(f"Max sentence length: {max_length}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

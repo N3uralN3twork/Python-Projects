68a7b654433dc6dd815162f94ecea9fc18a075df
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-11 17:26:30 -0400

    Update Testing.py

--- Sci-kit Learn/Testing.py
+++ Sci-kit Learn/Testing.py
@@ -1,29 +1,6 @@
 import pandas as pd
-from sklearn.preprocessing import OrdinalEncoder
-
-test = df
 
 
-Nominal = list(df.select_dtypes(include=['object', 'category']).columns)
-# Remove the ordinal and response variables, one by one
-Nominal.remove("Income")
-Nominal.remove("Seniority")
-# One-hot encode the nominal variables
-test = pd.get_dummies(test, drop_first = True, columns = Nominal)
 
 
-def OrdinalEncode(data, OrdinalColumns: list):
-    #You have to make a list of your ordinal variables first
-    OE = OrdinalEncoder()
-    for feature in OrdinalColumns:
-        try:
-            data[feature] = OE.fit_transform(data[[feature]])
-        except:
-            print(f"Error encoding {feature}")
-    return data
-
-Ordinal = ["Seniority"] #This is where you make a list of your ordinal features.
-test = OrdinalEncode(data=test, OrdinalColumns=Ordinal)
-
-OE = OrdinalEncoder()
-test["Seniority"] = OE.fit_transform(test[["Seniority"]])
+def Overtime(df)

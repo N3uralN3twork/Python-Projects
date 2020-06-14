7e392213682a8ac26300899182718474d86bf349
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-13 21:52:50 -0400

    Update Testing.py

--- NLP/Testing.py
+++ NLP/Testing.py
@@ -2,10 +2,17 @@
 
 
 
+import tensorflow as tf
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import re
+import seaborn as sns
 
-
-
-
-
-
-
+module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+model = hub.load(module_url)
+print ("module %s loaded" % module_url)
+def embed(input):
+  return model(input)

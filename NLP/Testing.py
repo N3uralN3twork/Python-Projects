35d6a5a6bde6611e7b061093ba008693afdd8bb4
Matthias Quinn <53357992+N3uralN3twork@users.noreply.github.com>
2020-06-20 15:58:58 -0400

    Update Testing.py

--- NLP/Testing.py
+++ NLP/Testing.py
@@ -2,28 +2,5 @@
 
 
 
-import tensorflow as tf
-from tensorflow import keras
-import tensorflow_datasets as tfds
-import numpy as np
-print(tf.__version__)
-
-dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
-                          as_supervised=True)
-train_dataset, test_dataset = dataset['train'], dataset['test']
-
-encoder = info.features["text"].encoder
-
-print(f"Vocabulary size: {encoder.vocab_size}")
-
-BUFFER_SIZE = 10000
-BATCH_SIZE = 4
-
-train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.padded_batch(BATCH_SIZE)
-
-test_dataset = test_dataset.padded_batch(BATCH_SIZE)
-
-
 
 
